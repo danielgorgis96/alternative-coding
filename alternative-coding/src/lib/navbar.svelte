fab0c6c943c858338448947ed6dc682911366956
Daniel Gorgis <daniel.gorgis-cic@ibm.com>
2022-02-20 11:15:12 +0100

    added some packages related to design

---
+++ alternative-coding/src/lib/navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+import {Navbar} from 'spaper'; 
+</script>
+
+<Navbar>
+    <h3 slot="brand">
+      <a href="#">AltoCode</a>
+    </h3>
+    <ul class="inline">
+      <li><a href="#">Documentation</a></li>
+      <li><a href="#">About</a></li>
+      <li>
+      </li>
+    </ul>
+  </Navbar>
--- alternative-coding/src/routes/index.svelte
+++ alternative-coding/src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import { Button } from 'spaper';
+import Navbar from '$lib/navbar.svelte';
+</script>
+
+<Navbar></Navbar>
+<Button size="large">Large</Button>
